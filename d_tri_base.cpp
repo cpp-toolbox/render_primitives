e53a11d7daf9f2c0403bf57d11e02a22535f799d
cuppajoeman <ccn@cuppajoeman.com>
2024-04-14 03:57:20 -0400

    clangd refactor

--- d_tri_base.cpp
+++ d_tri_base.cpp
@@ -1,7 +1,3 @@
-//
-// Created by ccn on 30/03/24.
-//
-
 #include <cassert>
 #include "d_tri_base.hpp"
 
@@ -24,19 +20,20 @@ void DTriBase::bind_index_vertex_data_to_opengl_for_later_use() const {
     glBufferData(GL_ARRAY_BUFFER, this->num_flattened_vertices * sizeof(float), flattened_vertices, GL_STATIC_DRAW);
 }
 
-
 void DTriBase::bind_vertex_attribute_interpretation_to_opengl_for_later_use() const {
     // vertex positions
     GLuint position_attribute_location = glGetAttribLocation(shader_pipeline.shader_program_id, "position");
     glEnableVertexAttribArray(position_attribute_location);
-    glVertexAttribPointer(position_attribute_location, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glVertexAttribPointer(position_attribute_location, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 }
 
 /**
  * precondition: opengl must be configured first
  */
-DTriBase::DTriBase(unsigned int draw_mode, float flattened_vertices[], int num_flattened_vertices, float r, float g, float b) {
-    this->shader_pipeline.load_in_shaders_from_file("../graphics/shaders/absolute_position.vert", "../graphics/shaders/absolute_position_with_color_uniform.frag");
+DTriBase::DTriBase(unsigned int draw_mode, float flattened_vertices[], int num_flattened_vertices, float r, float g,
+                   float b) {
+    this->shader_pipeline.load_in_shaders_from_file("../graphics/shaders/absolute_position.vert",
+                                                    "../graphics/shaders/absolute_position_with_color_uniform.frag");
     this->set_color_in_shader(r, g, b);
     assert(num_flattened_vertices % 3 == 0);
     this->num_vertices = num_flattened_vertices / 3;
@@ -45,7 +42,6 @@ DTriBase::DTriBase(unsigned int draw_mode, float flattened_vertices[], int num_f
     this->update_vertices(flattened_vertices, num_flattened_vertices);
     this->bind_vertex_attribute_interpretation_to_opengl_for_later_use();
 }
-
 
 void DTriBase::update_vertices(float flattened_vertices[], int num_flattened_vertices) {
     this->flattened_vertices = flattened_vertices;
